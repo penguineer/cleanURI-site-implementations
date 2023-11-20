0cbcaedfb12494bdcf3d1120a4990309dd6bed52
Stefan Haun <mail@tuxathome.de>
2023-11-21 00:35:53 +0100

    Add Reichelt extractor features

---
+++ src/test/java/com/penguineering/cleanuri/site/implementations/reichelt/TestProductDataExtractor.java
@@ -0,0 +1,59 @@
+package com.penguineering.cleanuri.site.implementations.reichelt;
+
+import com.penguineering.cleanuri.site.data.ProductDescription;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestProductDataExtractor {
+    /**
+     * This CSV file is used for testing the ProductDataExtractor class.
+     * Each row in the CSV file corresponds to one test case.
+     * The columns in the CSV file are as follows:
+     *
+     * <ul>
+     *     <li><b>fileName</b>: The name of the HTML file to test. This should be a relative path to the file from the classpath root.</li>
+     *     <li><b>expectedId</b>: The expected product id. This should be extracted from the h2 element inside the div#av_articleheader element in the HTML file.</li>
+     *     <li><b>expectedName</b>: The expected product name. This should be extracted from the span[itemprop=name] element inside the div#av_articleheader element in the HTML file.</li>
+     *     <li><b>expectedImageStr</b>: The expected product image URL. This should be extracted from the src attribute of the img[itemprop=image] element in the HTML file.</li>
+     * </ul>
+     */
+    @ParameterizedTest
+    @CsvSource({
+            "'testdata/reichelt/reichelt-p10228.html', 'LED 3MM RT', 'LED, 3 mm, bedrahtet, rot, 191 mcd, 50°', 'https://cdn-reichelt.de/bilder/web/artikel_ws/A500/LED3ST.jpg'",
+            "'testdata/reichelt/reichelt-p58170.html', 'DS 18B20Z', 'Digital Thermometer, 1-wire +/- 0,5°C, SO-8', 'https://cdn-reichelt.de/bilder/web/artikel_ws/A200/MSOP-8.jpg'"
+    })
+    public void testExtractProductDescription(String fileName,
+                                              String expectedId,
+                                              String expectedName,
+                                              String expectedImageStr) throws IOException {
+        Optional<String> expectedIdOpt = Optional.ofNullable(expectedId);
+        Optional<String> expectedNameOpt = Optional.ofNullable(expectedName);
+        Optional<URI> expectedImageOpt = Optional.ofNullable(expectedImageStr).map(URI::create);
+
+        URL resourceUrl = getClass().getClassLoader().getResource(fileName);
+        assertNotNull(resourceUrl, "Test data file not found");
+        File testDataFile = new File(resourceUrl.getFile());
+        Document doc = Jsoup.parse(testDataFile, "UTF-8", "");
+
+        ProductDataExtractor productDataExtractor = new ProductDataExtractor(doc);
+
+        Optional<ProductDescription> productDescriptionOpt = productDataExtractor.extractProductDescription();
+
+        assertTrue(productDescriptionOpt.isPresent());
+
+        ProductDescription productDescription = productDescriptionOpt.get();
+        assertEquals(expectedIdOpt, productDescription.getId());
+        assertEquals(expectedNameOpt, productDescription.getName());
+        assertEquals(expectedImageOpt, productDescription.getImage());
+    }
+}
---
+++ src/test/java/com/penguineering/cleanuri/site/implementations/reichelt/TestReicheltExtractor.java
@@ -0,0 +1,57 @@
+package com.penguineering.cleanuri.site.implementations.reichelt;
+
+import com.penguineering.cleanuri.site.data.Pricing;
+import com.penguineering.cleanuri.site.data.ProductDescription;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestReicheltExtractor {
+    /**
+     * This is a parameterized test that uses CSV source as input data.
+     * The CSV source contains four columns:
+     *
+     * <ul>
+     *   <li>fileName: The relative path to the HTML file to be parsed.</li>
+     *   <li>expectedTitle: The expected title of the document.</li>
+     *   <li>isProductDescriptionAvailable: A boolean indicating whether a product description is expected to be present.</li>
+     *   <li>isPricingAvailable: A boolean indicating whether pricing information is expected to be present.</li>
+     * </ul>
+     *
+     * Each row in the CSV source represents a separate test case.
+     */
+    @ParameterizedTest
+    @CsvSource({
+            "'testdata/reichelt/reichelt-p10228.html', 'LED 3MM RT: LED, 3 mm, bedrahtet, rot, 191 mcd, 50° bei reichelt elektronik', true, true",
+            "'testdata/reichelt/reichelt-p58170.html', 'DS 18B20Z: Digital Thermometer, 1-wire + - - 0,5°C, SO-8 bei reichelt elektronik', true, true"
+    })
+    public void testReicheltExtractor(String fileName,
+                                      String expectedTitle,
+                                      boolean isProductDescriptionAvailable,
+                                      boolean isPricingAvailable) throws IOException {
+        URL resourceUrl = getClass().getClassLoader().getResource(fileName);
+        assertNotNull(resourceUrl, "Test data file not found");
+        File file = new File(resourceUrl.getFile());
+        Document doc = Jsoup.parse(file, "UTF-8");
+
+        ReicheltExtractor reicheltExtractor = new ReicheltExtractor(doc);
+
+        Optional<String> titleOpt = reicheltExtractor.extractDocumentTitle();
+        assertEquals(expectedTitle, titleOpt.orElse(null));
+
+        Optional<ProductDescription> productDescriptionOpt = reicheltExtractor.extractProductDescription();
+        assertEquals(isProductDescriptionAvailable, productDescriptionOpt.isPresent());
+
+        Optional<Pricing> pricingOpt = reicheltExtractor.extractPricing();
+        assertEquals(isPricingAvailable, pricingOpt.isPresent());
+    }
+}
