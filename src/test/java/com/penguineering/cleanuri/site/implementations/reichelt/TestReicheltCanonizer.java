f872a9399742819ecaabaf31736770c22d2e3310
Stefan Haun <mail@tuxathome.de>
2023-11-21 00:35:36 +0100

    Add Reichelt canonizer features

---
+++ src/test/java/com/penguineering/cleanuri/site/implementations/reichelt/TestReicheltCanonizer.java
@@ -0,0 +1,41 @@
+package com.penguineering.cleanuri.site.implementations.reichelt;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.net.URI;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class TestReicheltCanonizer {
+
+    /**
+     * This is a parameterized test that uses CSV source as input data.
+     * The CSV source contains two columns:
+     *
+     * <ul>
+     *   <li>inputUri: The URI of the HTML page to be canonized.</li>
+     *   <li>outputUri: The expected canonized URI. It's null if the URI cannot be parsed by this canonizer.</li>
+     * </ul>
+     *
+     * Each row in the CSV source represents a separate test case.
+     */
+    @ParameterizedTest
+    @CsvSource({
+            "'https://www.reichelt.de/digital-thermometer-1-wire--0-5-c-so-8-ds-18b20z-p58170.html', 'https://www.reichelt.de/index.html?ARTICLE=58170'",
+            "'https://www.reichelt.de/non-parsable-uri', ''"
+    })
+    void testCanonize(String inputUri, String expectedOutputUri) {
+        ReicheltCanonizer canonizer = new ReicheltCanonizer(URI.create(inputUri));
+        Optional<URI> result = canonizer.canonize();
+
+        if (expectedOutputUri.isEmpty()) {
+            assertEquals(Optional.empty(), result);
+        } else {
+            assertTrue(result.isPresent());
+            assertEquals(expectedOutputUri, result.get().toString());
+        }
+    }
+}
